c77aa9ff33a15d5b3d2165c46839a6cab3b7b7fd
guoxi <you@example.com>
2018-12-23 00:19:40 +0800

    优化合并有序数组

--- recursion/mergeOrderedArray.js
+++ recursion/mergeOrderedArray.js
@@ -3,24 +3,28 @@
 */
 
 const mergeOrderedArray = (arr1, arr2) => {
-  let _arr = [],
+  let temp = [],
       length1 = arr1.length,
       length2 = arr2.length
   const movePointer = (i = 0, j = 0) => {
-    if (i < length1 || j < length2) {
-      if (j >= length2 || arr1[i] <= arr2[j]) {
-        _arr.push(arr1[i])
+    if (i < length1 && j < length2) {
+      if (arr1[i] <= arr2[j]) {
+        temp.push(arr1[i])
         movePointer(++i, j)
-      } else if (i >= length1 || arr2[j] < arr1[i]) {
-        _arr.push(arr2[j])
+      } else if (arr2[j] < arr1[i]) {
+        temp.push(arr2[j])
         movePointer(i, ++j)
       }
+    } else {
+      temp.concat(arr1.slice(i), arr2.slice(j))
     }
   }
   movePointer()
+  return temp
 }
 
-// const arr1 = [11, 16, 22,23, 25, 67, 68, 69]
-// const arr2 = [12, 13, 14, 15, 28, 33, 54, 65, 70]
+/* const arr1 = [11, 16, 22,23, 25, 67, 68, 69]
+ const arr2 = [12, 13, 14, 15, 28, 33, 54, 65, 70]
 
-// merginOrderedArray(arr1, arr2)
+ const result = mergeOrderedArray(arr1, arr2)
+ console.log(result)*/
